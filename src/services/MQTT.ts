8efe4d1d08a8d4284d04646325c8b3f46429ee8a
jannejki <jannekinnunen@hotmail.com>
2024-05-12 10:23:33 +0300

    Created MQTTMEssageHandler

--- src/services/MQTT.ts
+++ src/services/MQTT.ts
@@ -1,8 +1,5 @@
 import mqtt from 'mqtt';
-import knexInstance from '../config/knex';
-import { parseDevice, parseStatus } from '../utils/parseDevice';
-import { getCurrentPrice } from '../utils/priceUpdater';
-import { Device, Relay } from '../typings/database/dto/Device.dto';
+import MQTTMessageHandler from '../utils/MQTTMessageHandler';
 
 //======================================================//
 //                    variables                         //
@@ -30,53 +27,20 @@ const pricePattern = /^electric\/price$/;
 async function handleMessage(topic: string, message: Buffer): Promise<void> {
     try {
         if (newDevicePattern.test(topic)) {
-            const foundDeviceData = await knexInstance('devices').select('*').where('uuid', message.toString()).first();
-
-            if (foundDeviceData) {
-                let device = parseDevice(foundDeviceData);
-                console.log("New device connected: ", device.uuid);
-
-                // send a command to the device to turn on the relay
-                if (!process.env.MQTT_COMMAND_TOPIC) throw new Error("MQTT_TOPIC not set");
-
-                let topic = process.env.MQTT_COMMAND_TOPIC.replace("<uuid>", device.uuid);
-                let currentPrice = await getCurrentPrice();
-
-                device.relays.forEach((relay, i) => {
-                    let message = `relay=${i}&mode=${relay.mode}${relay.mode == 'manual' ? `&state=${relay.state}` : ''}&threshold=${relay.threshold}&price=${currentPrice}`;
-                    sendMQTTMessage(topic, message);
-                });
-            }
+            MQTTMessageHandler.newDevice(message.toString());
             return; // no need to process further
         }
 
         if (commandPattern.test(topic)) {
-        //    console.log("Command received: ", message.toString());
+            //    console.log("Command received: ", message.toString());
             return; // no need to process further
         }
 
 
         if (statusPattern.test(topic)) {
-            // Parse the uuid from the topic
             let uuid = topic.match(statusPattern)?.[1];
-
-            const foundDeviceData = await knexInstance('devices').select('*').where({ uuid }).first();
-            if (!foundDeviceData) throw new Error(`Status message from unknown device: ${message.toString()}`);
-
-            let device = parseDevice(foundDeviceData);
-            let relay = parseStatus(message.toString());
-
-            if (relayHasChanged(device, relay) == false) return;
-
-            // Postgresql arrays start from 1, not 0 so we need to add 1 to the relay number
-            let updateQuery = `
-            UPDATE devices
-            SET relays[${relay.relay + 1}] = ('${relay.mode}', '${relay.state}', ${relay.threshold})
-            WHERE uuid = '${uuid}'
-        `;
-
-            // Execute the raw SQL query
-            await knexInstance.raw(updateQuery);
+            if (!uuid) throw new Error("UUID not found in topic");
+            MQTTMessageHandler.status(uuid, message.toString());
             return; // no need to process further
         }
 
@@ -161,14 +125,3 @@ export function sendMQTTMessage(topic: string, message: string): Boolean {
 }
 
 
-/**
- * @brief checks if relay has changed. Compares a single relay object against the device object's relay
- * @param device {Device} - the device object
- * @param relay  {Relay} - the relay object
- * @returns {Boolean} - true if the relay has changed, false otherwise
- */
-function relayHasChanged(device: Device, relay: Relay): boolean {
-    return device.relays[relay.relay]?.mode == relay.mode
-        && device.relays[relay.relay]?.state == relay.state
-        && device.relays[relay.relay]?.threshold == relay.threshold;
-}
---
+++ src/utils/MQTTMessageHandler.ts
@@ -0,0 +1,78 @@
+import knexInstance from "../config/knex";
+import { sendMQTTMessage } from "../services/MQTT";
+import { Device, Relay } from "../typings/database/dto/Device.dto";
+import { parseDevice, parseStatus } from "./parseDevice";
+import { getCurrentPrice } from "./priceUpdater";
+
+const MQTTMessageHandler = {
+
+    /**
+     * @brief Handles a new device connecting to the MQTT broker
+     * @param uuid - the device that has connected to the MQTT broker
+     */
+    async newDevice(uuid: string): Promise<void> {
+        const foundDeviceData = await knexInstance('devices').select('*').where('uuid', uuid).first();
+        if (foundDeviceData) {
+            let device = parseDevice(foundDeviceData);
+            console.log("New device connected: ", device.uuid);
+
+            // send a command to the device to turn on the relay
+            if (!process.env.MQTT_COMMAND_TOPIC) throw new Error("MQTT_TOPIC not set");
+
+            let topic = process.env.MQTT_COMMAND_TOPIC.replace("<uuid>", device.uuid);
+            let currentPrice = await getCurrentPrice();
+
+            device.relays.forEach((relay, i) => {
+                let message = `relay=${i}&mode=${relay.mode}${relay.mode == 'manual' ? `&state=${relay.state}` : ''}&threshold=${relay.threshold}&price=${currentPrice}`;
+                sendMQTTMessage(topic, message);
+            });
+        }
+    },
+
+
+    /**
+     * @brief Checks if the status message is from a known device and checks if the relay has changed. If it has, updates the database
+     * @param uuid the UUID of the device that has sent the status message
+     * @param message the status message from the device
+     * @returns {Promise<void>}
+     */
+    async status(uuid: string, message: string): Promise<void> {
+        // Parse the uuid from the topic
+        const foundDeviceData = await knexInstance('devices').select('*').where({ uuid }).first();
+        if (!foundDeviceData) throw new Error(`Status message from unknown device: ${message}`);
+
+        let device = parseDevice(foundDeviceData);
+        let relay = parseStatus(message.toString());
+
+        if (relayHasChanged(device, relay) == false) return;
+
+        // Postgresql arrays start from 1, not 0 so we need to add 1 to the relay number
+        let updateQuery = `
+                    UPDATE devices
+                    SET relays[${relay.relay + 1}] = ('${relay.mode}', '${relay.state}', ${relay.threshold})
+                    WHERE uuid = '${uuid}'
+                `;
+
+        // Execute the raw SQL query
+        await knexInstance.raw(updateQuery);
+    }
+}
+
+export default MQTTMessageHandler;
+
+
+//=================================================//
+//         Helper functions for MQTTMessageHandler  //
+//=================================================//
+
+/**
+ * @brief checks if relay has changed. Compares a single relay object against the device object's relay
+ * @param device {Device} - the device object
+ * @param relay  {Relay} - the relay object
+ * @returns {Boolean} - true if the relay has changed, false otherwise
+ */
+function relayHasChanged(device: Device, relay: Relay): boolean {
+    return device.relays[relay.relay]?.mode == relay.mode
+        && device.relays[relay.relay]?.state == relay.state
+        && device.relays[relay.relay]?.threshold == relay.threshold;
+}
